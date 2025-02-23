7b34f4da2432cccc14cfc5a73eca01e118491ea4
Tristan Tong <ttong2@buffalo.edu>
2025-02-23 17:30:27 -0500

    Created flag reader

--- csv.c
+++ csv.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (int argc, char* argv[]) {
 
-    if (argc > 6) {
-        printf("Invalid number of arguments: expected maximum 6 but got %d\n", argc);
+    if (argc > 11) {
+        printf("Invalid number of arguments: expected maximum 11 but got %d\n", argc);
         return 1;
     }
 
@@ -35,10 +36,47 @@ int main (int argc, char* argv[]) {
     }
     //CODE GOES HERE
 
+    
+    
+    printf("File opened: %s\n", filename);
 
-    printf("%s\n", filename);
+    char* firstflag;
+    char* min;
+    char* max;
+    char* mean;
+    char* recordfield;
+    char* recordvalue;
 
     
+    for (int i = 1; i < argc - 1; i++) {
+        if (i == 1) {
+            if (strcmp(argv[1], "-f") == 0) {
+                firstflag = "f";
+            } else if (strcmp(argv[1], "-r") == 0) {
+                firstflag = "r";
+            } else if (strcmp(argv[1], "-h") == 0) {
+                firstflag = "h";
+            } else {
+                firstflag = "0";
+            }
+        }
+        if (strcmp(argv[i], "-min") == 0) {
+            min = argv[i + 1];
+            i++;
+        } else if (strcmp(argv[i], "-max") == 0) {
+            max = argv[i + 1];
+            i++;
+        } else if (strcmp(argv[i], "-mean") == 0) {
+            mean = argv[i + 1];
+            i++;
+        } else if (strcmp(argv[i], "-records") == 0) {
+            recordfield = argv[i + 1];
+            recordvalue = argv[i + 2];
+            i += 2;
+        }
+    }
+
+    printf("Flags read. Firstflag: %s, min: %s, max: %s, mean: %s, recordfield: %s, recordvalue: %s", firstflag, min, max, mean, recordfield, recordvalue);
     
 
     if (fclose (filepointer) == 0) {
